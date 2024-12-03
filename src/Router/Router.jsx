ea2ec167638ec93ae5e218e61173aaee53a9e0d9
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2024-12-03 21:30:02 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../Layout/HomeLayout";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomeLayout></HomeLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
